cc1af7eb189fcf95e213b9bcfb22b843b9d57997
A S M Nasim <54743104+abusalehmdnasim6629@users.noreply.github.com>
2020-06-27 02:35:10 +0600

    Update topbar.blade.php

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,4 +1,14 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+.lnk:hover {
+  /* border-top: 2px solid #38b9f8; */
+  box-shadow:0 2px 5px rgb(0,1.0,1.0);
+  border: 2px solid #38b9f8;
+  border-radius:10px;
+  color:#38b9f8;
+  
+  /* padding: 6px 0;  */
+  
+}
 <header class="header">
     <div class="container">
         <div class="main-navigation">
@@ -15,44 +25,44 @@
                     <!-- <button class="navbar-toggler toggler-spring"><span class="navbar-toggler-icon"></span></button> -->
                     <div class="collapse navbar-collapse justify-content-sm-end">
                         <ul class="navbar-nav navbar-mobile ">
-                            <li class="nav-item mr-2"><a class="nav-link smooth-menu " data-toggle="popover" title="Home" id ="home" href="{{URL::to('/')}}" style="">
+                            <li class="nav-item mr-2"><a class="nav-link smooth-menu lmk" data-toggle="popover" title="Home" id ="home" href="{{URL::to('/')}}" style="">
                             <i class="fa fa-home"></i>
                             Home
                              </a>
                             </li>
                             @if (request()->is('/'))
-                            <li class="nav-item "><a class="nav-link smooth-menu" href="#about">
+                            <li class="nav-item "><a class="nav-link smooth-menu lnk" href="#about">
                             <i class="fa fa-info-circle"></i>
                             About</a></li>
                             @endif
 
                             @if (request()->is('/'))
-                            <li class="nav-item "><a class="nav-link smooth-menu" href="#service">
+                            <li class="nav-item "><a class="nav-link smooth-menu lnk" href="#service">
                             <i class="fa fa-gavel"></i>
                             Services</a></li>
                             @endif
 
-                            <li class="nav-item "><a class="nav-link smooth-menu"
+                            <li class="nav-item "><a class="nav-link smooth-menu lnk"
                                     href="{{request()->is('/') ? '#history' : url('full-history')}}">
                                     <i class="fa fa-history"></i>
                                     History</a></li>
-                            <li class="nav-item"><a class="nav-link smooth-menu"
+                            <li class="nav-item"><a class="nav-link smooth-menu lnk"
                                     href="{{request()->is('/') ? '#event' : url('view-all-event')}}">
                                     <i class="fa fa-calendar"></i>
                                     Event</a></li>
-                            <li class="nav-item"><a class="nav-link smooth-menu" href="{{url('/career')}}">
+                            <li class="nav-item"><a class="nav-link smooth-menu lnk" href="{{url('/career')}}">
                             <i class="fa fa-university"></i>
                             Career</a>
                             </li>
-                            <li class="nav-item"><a class="nav-link smooth-menu"
+                            <li class="nav-item"><a class="nav-link smooth-menu lnk"
                                     href="{{request()->is('/') ? '#gallary' : url('view-all')}}">
                                     <i class="fa fa-image"></i>
                                     Gallery</a></li>
-                            <li class="nav-item"><a class="nav-link smooth-menu"
+                            <li class="nav-item"><a class="nav-link smooth-menu lnk"
                                     href="{{URL::to('/blog')}}">
                                     <i class="fa fa-book"></i>
                                     Blog</a></li>
-                              <li class="nav-item"><a class="nav-link smooth-menu" href="{{url('/bunnon-basket')}}">
+                              <li class="nav-item"><a class="nav-link smooth-menu lnk" href="{{url('/bunnon-basket')}}">
                            
                             Order</a>
                             </li>        
@@ -60,19 +70,19 @@
 								$lg = Session::get('lcheck');
 								if($lg!=null){								
 								?>
-                            <li class="nav-item "><a class="nav-link smooth-menu"
+                            <li class="nav-item "><a class="nav-link smooth-menu lnk"
                                     href="{{URL::to('/profile')}}">
                                     <i class="fa fa-user"></i>
                                     Profile</a></li>
 
-                            <li class="nav-item "><a class="nav-link smooth-menu"
+                            <li class="nav-item "><a class="nav-link smooth-menu lnk"
                                     href="{{URL::to('/logout')}}" >
                                     <i class="fa fa-power-off"></i>
                                     Logout</a></li>
 
                             <?php }else{ ?>
-                            <li class="nav-item "><a class="nav-link" data-toggle="modal" data-target="#exampleModal">Login</a></li>
-                            <li class="nav-item"><a class="nav-link smooth-menu"
+                            <li class="nav-item lnk"><a class="nav-link" data-toggle="modal" data-target="#exampleModal">Login</a></li>
+                            <li class="nav-item lnk"><a class="nav-link smooth-menu"
                                     href="{{URL::to('/member-registration')}}">Member</a></li>
                             <?php } ?>
 
