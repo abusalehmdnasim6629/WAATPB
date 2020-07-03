f5478e02c7a32e7af9ccc8f315d4756c1444314c
A S M Nasim <54743104+abusalehmdnasim6629@users.noreply.github.com>
2020-07-03 14:32:34 +0600

    Update order_detail.blade.php

--- resources/views/admin/order_detail.blade.php
+++ resources/views/admin/order_detail.blade.php
@@ -22,8 +22,7 @@
 						<table class="table table-condensed">
 							  <thead>
 								  <tr>
-								      <th>Order Id</th>
-								      <th>Category</th>
+									  <th>Order Id</th>
 									  <th>Name</th>
 									  <th>City</th>
 									  <th>Address</th>
@@ -32,16 +31,16 @@
 								  </tr>
 							  </thead>   
 							  <tbody>
-                                
+                                @foreach($result->unique('order_id') as $result)
                                   <tr>
-								<td class="center">{{ $result->order_id }} </td>
-								<td class="center">{{ $result->category }} </td>
+                                <td class="center">{{ $result->order_id }}</td>
+                                
                                 <td class="center">{{ $result->name }}</td>
                                 <td class="center">{{ $result->city }}</td>
                                 <td class="center">{{ $result->address }}</td>  
                                 <td class="center">{{ $result->phone }}</td>                                  
 								</tr>
-                               
+                               @endforeach
 								
 								                                 
 							  </tbody>
@@ -64,21 +63,27 @@
 						<table class="table table-bordered table-striped table-condensed">
 							  <thead>
 								  <tr>
+									 
 									  
-									  <th>Product name</th>
+									  <th>Product Name</th>
+									  <th>Category</th>
+									  <th>Brand</th>
 									  <th>Measurement</th>
+									  <th>Notes</th>
+
 									                                         
 								  </tr>
 							  </thead>   
 							  <tbody>
-                             
+                              <?php foreach($result2 as $result) { ?>
                                 <tr>
-                                
-                                <td class="center">{{ $result->type }} </td>  
-                                <td class="center">{{ $result->measurement }} </td> 
-                                                            
+                                <td class="center">{{ $result->product_name }} </td>
+                                <td class="center">{{ $result->category }} </td>  
+                                <td class="center">{{ $result->brand }} </td>  
+                                <td class="center">{{ $result->quantity }} </td> 
+                                <td class="center">{{ $result->note }} </td>                            
 								</tr>
-                               
+                              <?php } ?>
                              
 								                         
 							  </tbody>
